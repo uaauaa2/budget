502cadd607fbe224181761a96f93d264958f80ce
uaauaa2 <uaauaa2@yandex.ru>
2015-09-22 19:59:38 +0600

    new Overview page

--- www/js/overview-controller.js
+++ www/js/overview-controller.js
@@ -1,5 +1,7 @@
 angular.module('budget.controllers').controller('OverviewCtrl', function($scope, $http, dataService) {
-    $scope.expenseItems = [];
+    $scope.allExpenseItems = [];
+    $scope.expItems = {};
+
     $scope.auth = {code: "", token: ""};  
     
     $scope.stringdb = "";
@@ -8,8 +10,6 @@ angular.module('budget.controllers').controller('OverviewCtrl', function($scope,
     
 
     $scope.init = function() {
-        $scope.expenseItems = dataService.db.queryAll("expenseItems");
-        
         
     };
     
@@ -51,6 +51,72 @@ angular.module('budget.controllers').controller('OverviewCtrl', function($scope,
     $scope.getAuthToken = function(){
         dataService.getAuthToken($scope.auth.code); 
     }
+
+    
+    $scope.initLatestExpenses = function () {
+        var result = {};
+        var add = function (expense) {
+            var dateElementTo = result[expense.date];
+
+            if (!dateElementTo) {
+                dateElementTo = { date: expense.date, totalAmount: 0, expenses: {} };
+                result[expense.date] = dateElementTo;
+            }
+            dateElementTo.totalAmount += expense.amount;
+
+            var expenseItemElementTo = dateElementTo.expenses[expense.expenseItemId];
+
+            if (!expenseItemElementTo) {
+                expenseItemElementTo = {
+                    expenseItemId: expense.expenseItemId,
+                    expenseItemName: $scope.expItems[expense.expenseItemId].name,
+                    amount: expense.amount,
+                    comment: expense.comment
+                }
+                dateElementTo.expenses[expense.expenseItemId] = expenseItemElementTo;
+            }
+            else {
+                expenseItemElementTo.amount += expense.amount;
+                if (expenseItemElementTo.comment)
+                    expenseItemElementTo.comment += expense.comment + "; ";
+            }
+        }
+
+
+        $scope.db = dataService.getDB();
+
+        $scope.allExpenseItems = $scope.db.queryAll("expenseItems", { sort: [["orderNum", "ASC"]] });
+        $scope.allExpenseItems.forEach(function (element) {
+            $scope.expItems[element.ID] = element;
+        }, this);
+
+        var expenses = $scope.db.queryAll("expenses", { sort: [["date", "DESC"]], distinct: ["date"] });
+        var dateFrom = new Date(expenses[2].date);
+
+        expenses = $scope.db.queryAll("expenses", {
+            query: function (row) {
+                var d = new Date(row.date);
+                if (d >= dateFrom)
+                    return true;
+                else
+                    return false;
+            }
+        });
+
+
+        for (var k = 0; k < expenses.length; k++) {
+            add(expenses[k]);
+        }
+
+        result.days = Object.keys(result);
+        result.days.sort();
+        result.days.reverse();
+
+        return result;
+    }
+
+    $scope.latestExpenses = $scope.initLatestExpenses();
+
     
  
   
