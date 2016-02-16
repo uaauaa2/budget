e41e9c782f05242a59bb675ab171971286571079
uaauaa2 <uaauaa2@yandex.ru>
2016-02-16 20:35:40 +0600

    urls fixing for github

--- www/web_app.js
+++ www/web_app.js
@@ -7,62 +7,62 @@ angular.module('budget', ['ui.router', 'budget.controllers', 'budget.services'])
         
             .state('index', {
                 url: '/index',
-                templateUrl : 'budget/www/templates/web/expenses.html',
-                //templateUrl : 'www/templates/web/expenses.html',
+                //templateUrl : 'budget/www/templates/web/expenses.html',
+                templateUrl : 'www/templates/web/expenses.html',
                 controller  : 'ExpensesCtrl'
             })
             
             .state('expensesTable', {
-                templateUrl : 'budget/www/templates/web/expensesTable.html',
-                //templateUrl : 'www/templates/web/expensesTable.html',
+                //templateUrl : 'budget/www/templates/web/expensesTable.html',
+                templateUrl : 'www/templates/web/expensesTable.html',
                 controller  : 'ExpensesTableCtrl'
             })
 
             .state('expenses', {
-                templateUrl : 'budget/www/templates/web/expenses.html',
-                //templateUrl : 'www/templates/web/expenses.html',
+                //templateUrl : 'budget/www/templates/web/expenses.html',
+                templateUrl : 'www/templates/web/expenses.html',
                 controller  : 'ExpensesCtrl'
             })
             
             .state('income', {
-                templateUrl : 'budget/www/templates/web/income.html',
-                //templateUrl : 'www/templates/web/income.html',
+                //templateUrl : 'budget/www/templates/web/income.html',
+                templateUrl : 'www/templates/web/income.html',
                 controller  : 'IncomeCtrl'
             })
             
             .state('expensesPlan', {
-                templateUrl : 'budget/www/templates/web/expensesPlan.html',
-                //templateUrl : 'www/templates/web/expensesPlan.html',
+                //templateUrl : 'budget/www/templates/web/expensesPlan.html',
+                templateUrl : 'www/templates/web/expensesPlan.html',
                 controller  : 'ExpensesPlanCtrl'
             })
             
             .state('balance', {
-                templateUrl : 'budget/www/templates/web/balance.html',
-                //templateUrl : 'www/templates/web/balance.html',
+                //templateUrl : 'budget/www/templates/web/balance.html',
+                templateUrl : 'www/templates/web/balance.html',
                 controller  : 'BalanceCtrl'
             })
             
             .state('editExpenseItems', {
-                templateUrl : 'budget/www/templates/web/editExpenseItems.html',
-                //templateUrl : 'www/templates/web/editExpenseItems.html',
+                //templateUrl : 'budget/www/templates/web/editExpenseItems.html',
+                templateUrl : 'www/templates/web/editExpenseItems.html',
                 controller  : 'EditExpenseItemsCtrl'
             })
 
             .state('settings', {
-                templateUrl : 'budget/www/templates/web/settings.html',
-                //templateUrl : 'www/templates/web/settings.html',
+                //templateUrl : 'budget/www/templates/web/settings.html',
+                templateUrl : 'www/templates/web/settings.html',
                 controller  : 'SettingsCtrl'
             })
             
             .state('plan', {
-                templateUrl : 'budget/www/templates/web/plan.html',
-                //templateUrl : 'www/templates/web/plan.html',
+                //templateUrl : 'budget/www/templates/web/plan.html',
+                templateUrl : 'www/templates/web/plan.html',
                 controller  : 'PlanCtrl'
             })
             
             .state('about', {
-                templateUrl : 'budget/www/templates/web/about.html',
-                //templateUrl : 'www/templates/web/about.html'
+                //templateUrl : 'budget/www/templates/web/about.html',
+                templateUrl : 'www/templates/web/about.html'
             })
             
             ;
